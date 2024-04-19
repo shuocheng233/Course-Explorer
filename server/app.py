dd4167ef7e6d2d7e1e62bf74df50bb3a3d2d23fa
Brad Zeng <brad.zeng2015@gmail.com>
2024-04-19 17:28:00 -0500

    Co-authored-by: michaelxu12345 <michaelxu12345@users.noreply.github.com>

--- server/app.py
+++ server/app.py
@@ -1,92 +1,66 @@
 # imports
 from flask import Flask, request, make_response
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sqlalchemy
+from sqlalchemy import text
+from yaml import load, Loader
+from flask_cors import CORS
 
-# initializing Flask app
+
+
+def init_connection_engine():
+    """ initialize database setup
+    Takes in os variables from environment if on GCP
+    Reads in local variables that will be ignored in public repository.
+    Returns:
+        pool -- a connection to GCP MySQL
+    """
+
+
+    # detect env local or gcp
+    if os.environ.get('GAE_ENV') != 'standard':
+        try:
+            variables = load(open("app.yaml"), Loader=Loader)
+        except OSError as e:
+            print("Make sure you have the app.yaml file setup")
+            os.exit()
+
+        env_variables = variables['env_variables']
+        for var in env_variables:
+            os.environ[var] = env_variables[var]
+
+    pool = sqlalchemy.create_engine(
+        sqlalchemy.engine.url.URL.create(
+            drivername="mysql+pymysql",
+            username=os.environ.get('MYSQL_USER'),
+            password=os.environ.get('MYSQL_PASSWORD'),
+            database=os.environ.get('MYSQL_DB'),
+            host=os.environ.get('MYSQL_HOST')
+        )
+    )
+    return pool
+
+
 app = Flask(__name__)
+CORS(app)
+db = init_connection_engine()
 
-# Google Cloud SQL (change this accordingly)
-PASSWORD ="your database password"
-PUBLIC_IP_ADDRESS ="public ip of database"
-DBNAME ="database name"
-PROJECT_ID ="gcp project id"
-INSTANCE_NAME ="instance name"
-
-# configuration
-app.config["SECRET_KEY"] = "yoursecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-
-db = SQLAlchemy(app)
-
-# User ORM for SQLAlchemy
-class Users(db.Model):
-	id = db.Column(db.Integer, primary_key = True, nullable = False)
-	name = db.Column(db.String(50), nullable = False)
-	email = db.Column(db.String(50), nullable = False, unique = True)
-
-@app.route('/add', methods =['POST'])
-def add():
-	# getting name and email
-	name = request.form.get('name')
-	email = request.form.get('email')
-
-	# checking if user already exists
-	user = Users.query.filter_by(email = email).first()
-
-	if not user:
-		try:
-			# creating Users object
-			user = Users(
-				name = name,
-				email = email
-			)
-			# adding the fields to users table
-			db.session.add(user)
-			db.session.commit()
-			# response
-			responseObject = {
-				'status' : 'success',
-				'message': 'Successfully registered.'
-			}
-
-			return make_response(responseObject, 200)
-		except:
-			responseObject = {
-				'status' : 'fail',
-				'message': 'Some error occurred !!'
-			}
-
-			return make_response(responseObject, 400)
-		
-	else:
-		# if user already exists then send status as fail
-		responseObject = {
-			'status' : 'fail',
-			'message': 'User already exists !!'
-		}
-
-		return make_response(responseObject, 403)
-
-@app.route('/view')
-def view():
-	# fetches all the users
-	users = Users.query.all()
-	# response list consisting user details
-	response = list()
-
-	for user in users:
-		response.append({
-			"name" : user.name,
-			"email": user.email
-		})
-
-	return make_response({
-		'status' : 'success',
-		'message': response
-	}, 200)
-
-
-if __name__ == "__main__":
-	# serving the app directly
-	app.run()
+@app.route("/")
+def homepage():
+    """ returns rendered homepage """
+    conn = db.connect()
+    query = "SELECT * FROM (SELECT r.Subject, COUNT(*) AS NumberOfCourses, AVG(r.Rating) AS AverageRating, COUNT(*) AS NumberOfFavorite FROM Rating r JOIN Favorite f ON r.Subject = f.Subject GROUP BY r.Subject) a ORDER BY Subject LIMIT 15;"
+    query_results = conn.execute(text(query)).fetchall()
+    conn.close()
+    # print(query_results)
+    ret = []
+    for result in query_results:
+        item = {
+            "subject": result[0],
+            "courses": result[1],
+            "average": result[2],
+            "favorite": result[3]
+        }
+        ret.append(item)
+    print(ret)
+    return ret
