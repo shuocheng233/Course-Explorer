332abef7d630bd6303debad1b9e893f800b86c6c
Peter Cheng <shuoc4@illinois.edu>
2024-05-20 17:57:05 -0500

    complete deployed version of server

--- server/config.py
+++ server/config.py
@@ -1,36 +1,39 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.engine import url
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv('ENVIRONMENT') == 'development':
+    from dotenv import load_dotenv
+    load_dotenv()
+else:
+    from google.cloud import secretmanager
 
-class Config:
-    MYSQL_USER = os.getenv('MYSQL_USER')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-    MYSQL_DB = os.getenv('MYSQL_DB')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_CONNECTION = os.getenv('MYSQL_CONNECTION')
-
-class Development(Config):
+class Development:
     @staticmethod
     def get_engine():
-        user = Config.MYSQL_USER
-        password = Config.MYSQL_PASSWORD
-        db = Config.MYSQL_DB
-        host = Config.MYSQL_HOST
+        user = os.getenv('MYSQL_USER')
+        password = os.getenv('MYSQL_PASSWORD')
+        db = os.getenv('MYSQL_DB')
+        host = os.getenv('MYSQL_HOST')
     
         return create_engine(
             f"mysql+pymysql://{user}:{password}@{host}/{db}"
         )
-    
-class Production(Config):
+
+class Production:
+    @staticmethod
+    def access_secret_version(secret_id):
+        client = secretmanager.SecretManagerServiceClient()
+        name = f"projects/cs411-cs411114/secrets/{secret_id}/versions/latest"
+        response = client.access_secret_version(request={"name": name})
+        return response.payload.data.decode('UTF-8')
+
     @staticmethod
     def get_engine():
-        user = Config.MYSQL_USER
-        password = Config.MYSQL_PASSWORD
-        db = Config.MYSQL_DB
-        connection = Config.MYSQL_CONNECTION
+        user = Production.access_secret_version('db-user')
+        password = Production.access_secret_version('db-password')
+        db = Production.access_secret_version('db-name')
+        connection = Production.access_secret_version('db-connection')
 
         return create_engine(
             url.URL.create(
